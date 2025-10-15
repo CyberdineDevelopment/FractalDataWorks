329d1629681bca6947546ca34adabea682c4ec3b
MikeBlair <cmblair@domesticateddata.com>
2025-10-14 22:35:29 -0500

    Add DataCommand item template
    Create template for generating new DataCommand classes with:
    - TypeOption attribute for TypeCollection discovery
    - Conditional base class (with/without TInput)
    - Category selection (Query/Insert/Update/Delete)
    - Optional expression properties (Filter/Projection/Ordering/Paging)
    - Unique ID assignment
    - Proper XML documentation
    
    Usage: dotnet new fractaldataworks-datacommand -n Upsert --UniqueId 5 --Category Insert --HasInputData true --HasFilter true
    
    Part of Data Architecture MVP implementation.

---
+++ templates/FractalDataWorks.ItemTemplates/DataCommand/DataCommandTemplate.cs
@@ -0,0 +1,74 @@
+using FractalDataWorks.Collections.Attributes;
+using FractalDataWorks.Commands.Data.Abstractions;
+
+namespace FractalDataWorks.Commands.Data;
+
+/// <summary>
+/// DataCommand command for [DESCRIBE OPERATION].
+/// </summary>
+/// <typeparam name="T">The type of entity.</typeparam>
+#if (HasInputData)
+[TypeOption(typeof(DataCommands), "DataCommand")]
+public sealed class DataCommandCommand<T> : DataCommandBase<TResult, TInput>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataCommandCommand{T}"/> class.
+    /// </summary>
+    /// <param name="containerName">The name of the container.</param>
+    /// <param name="data">The input data.</param>
+    public DataCommandCommand(string containerName, TInput data)
+        : base(
+            id: 99,
+            name: "DataCommand",
+            containerName,
+            DataCommandCategory.CommandCategory,
+            data)
+    {
+    }
+#else
+[TypeOption(typeof(DataCommands), "DataCommand")]
+public sealed class DataCommandCommand<T> : DataCommandBase<TResult>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataCommandCommand{T}"/> class.
+    /// </summary>
+    /// <param name="containerName">The name of the container.</param>
+    public DataCommandCommand(string containerName)
+        : base(
+            id: 99,
+            name: "DataCommand",
+            containerName,
+            DataCommandCategory.CommandCategory)
+    {
+    }
+#endif
+
+#if (HasFilter)
+    /// <summary>
+    /// Gets or sets the filter expression (WHERE clause).
+    /// </summary>
+    public IFilterExpression? Filter { get; init; }
+
+#endif
+#if (HasProjection)
+    /// <summary>
+    /// Gets or sets the projection expression (SELECT clause).
+    /// </summary>
+    public IProjectionExpression? Projection { get; init; }
+
+#endif
+#if (HasOrdering)
+    /// <summary>
+    /// Gets or sets the ordering expression (ORDER BY clause).
+    /// </summary>
+    public IOrderingExpression? Ordering { get; init; }
+
+#endif
+#if (HasPaging)
+    /// <summary>
+    /// Gets or sets the paging expression (SKIP/TAKE).
+    /// </summary>
+    public IPagingExpression? Paging { get; init; }
+
+#endif
+}
