0f9f3cd6be18903cb10c9c3ebc1e73348ca9646e
MikeBlair <cmblair@domesticateddata.com>
2025-10-17 16:39:39 -0500

    Remove stray PowerShell utility files

--- remove-non-mcp.ps1
+++
@@ -1,6 +0,0 @@
-$projects = dotnet sln list | Where-Object { $_ -match '.csproj' -and $_ -notmatch '[Mm][Cc][Pp]' }
-
-foreach ($proj in $projects) {
-    Write-Host "Removing: $proj"
-    dotnet sln remove $proj
-}
